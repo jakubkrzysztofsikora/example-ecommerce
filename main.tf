21d60498b21b3c15b8271c42db81dbdceecb3781
Apuroksiczek <82889581+Apuroksiczek@users.noreply.github.com>
2023-06-01 12:35:34 +0200

    Add files via upload

--- main.tf
+++ main.tf
@@ -6,3 +6,12 @@ resource "azurerm_resource_group" "rg" {
   name     = "MyResourceGroup"
   location = "East US"
 }
+
+terraform {
+  backend "azurerm" {
+    resource_group_name = "StorageAccount-ResourceGroup"
+    storage_account_name = "marcinstorage120397213"
+    container_name = "tfstate"
+    key = "prod.terraform.tfstate"
+  }
+}
