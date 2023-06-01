e67d40b2f5b67cb95d54e1ef3a9705012b00171c
Apuroksiczek <82889581+Apuroksiczek@users.noreply.github.com>
2023-06-01 13:00:54 +0200

    Add files via upload

--- main.tf
+++ main.tf
@@ -16,3 +16,20 @@ terraform {
     access_key = "hR+AEvrgdE5s8ZZtrfRjUqWhIB1VibJd9lQe9f9Dgs1CYtFPHFE4kxlDTjCnp98cYKhD4jibtDdU+AStIhiFCQ=="
   }
 }
+
+resource "azurerm_app_service_plan" "example" {
+  name                = "myAppServicePlan"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  sku {
+    tier = "Free"
+    size = "F1"
+  }
+}
+
+resource "azurerm_app_service" "example" {
+  name                = "myWebApp"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
+}
