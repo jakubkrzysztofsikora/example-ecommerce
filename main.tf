362735ba3e265cbd51161f3e2688b420b7671829
Apuroksiczek <82889581+Apuroksiczek@users.noreply.github.com>
2023-06-02 10:16:31 +0200

    Add files via upload

--- main.tf
+++ main.tf
@@ -60,4 +60,34 @@ resource "azurerm_sql_database" "example" {
   server_name         = azurerm_sql_server.example.name
   location            = azurerm_resource_group.rg.location
   edition             = "Basic"
+
+  site_config {
+    dotnet_framework_version = "v5.0"
+  }
+
+  app_settings = {
+    "ConnectionStrings:DefaultConnection" = "Server=tcp:${azurerm_sql_server.example.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_sql_database.example.name};Persist Security Info=False;User ID=${azurerm_sql_server.example.administrator_login};Password=${azurerm_sql_server.example.administrator_login_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+  }
+
+  connection_string {
+    name  = "DBConnectionString"
+    type  = "SQLAzure"
+    value = "Server=tcp:${azurerm_sql_server.example.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_sql_database.example.name};Persist Security Info=False;User ID=${azurerm_sql_server.example.administrator_login};Password=${azurerm_sql_server.example.administrator_login_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+  }
+
+}
+
+resource "azurerm_storage_account" "example" {
+name                     = "mystorageaccount2718342156"
+resource_group_name      = azurerm_resource_group.rg.name
+location = azurerm_resource_group.rg.location
+account_tier = "Standard"
+account_replication_type = "LRS"
+account_kind = "StorageV2"
+}
+
+resource "azurerm_storage_container" "example" {
+name = "mycontainer"
+storage_account_name = azurerm_storage_account.example.name
+container_access_type = "private"
 }
